08cd20894370c04d399df9b4b963d96fa7f04021
ezra.pratama <>
2021-10-20 01:40:20 +0700

    add view for stats

---
+++ resources/views/stats.blade.php
@@ -0,0 +1,39 @@
+<html lang="en">
+  <head>
+    <title>Keterisian Ibadah {{$next}}</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+
+    <div class="container p-5">
+        <h5>Keterisian Kuota Kursi Ibadah Minggu, {{$next}}</h5>
+
+        <div id="piechart" style="width: 900px; height: 500px;"></div>
+    </div>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+            ['Category', 'Quantity'],
+            ['Kuota', {{ $quota }}],
+            ['Terdaftar', {{ $registered }}]
+        ]);
+
+          var options = {
+            title: 'Keterisian Kuota',
+            is3D: false,
+          };
+
+          var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+          chart.draw(data, options);
+        }
+      </script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StatsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,5 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
+
+Route::get('/stats', [StatsController::class, 'index']);
